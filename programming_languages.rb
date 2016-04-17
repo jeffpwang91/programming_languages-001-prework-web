7e64345542b568329ece21c911751513678dbd4e
jeffpwang91 <jeffpwang91@gmail.com>
2016-04-17 18:53:48 +0000

    nitrous-backup at Sun Apr 17 18:53:48 UTC 2016

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,47 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  type_style = ''
+  style_hash = {}
+  languages.each do |style, languages|
+    languages.each do |language, type|
+      type_style << type.to_s
+    end
+    type_style << (style_hash[:style] = style).to_s
+  end
+  new_hash
+  binding.pry
 end
+
+reformat_languages({
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+    },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+  }
+}
+)
